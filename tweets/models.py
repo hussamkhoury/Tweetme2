a07e659be55520292a094e74f1e8b0a74a99c1c6
unknown <hussamalkhoury501@gmail.com>
2022-04-29 09:32:44 -0700

    fix tweet format and reverse tweet order (descending order)

--- tweets/models.py
+++ tweets/models.py
@@ -5,7 +5,9 @@ class Tweet(models.Model):
     # id = models.AutoField(PRIMARY_KEY=True) Automaticaly added by django
     content = models.TextField(blank=True, null=True) # null for DB, blank for django
     image = models.FileField(upload_to='images/', blank=True, null=True)
-
+    class Meta:
+        ordering = ['-id']
+        
     def serialize(self):
         return {
             "id": self.id,
