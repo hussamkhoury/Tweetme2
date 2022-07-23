c50ff3f3a7b8b2ede33aa0802aa9e19fa1e7eee3
unknown <hussamalkhoury501@gmail.com>
2022-07-23 03:37:27 -0700

    Link users to tweets

--- tweets/models.py
+++ tweets/models.py
@@ -1,8 +1,11 @@
 import random
+from django.conf import settings
 from django.db import models
 
+User = settings.AUTH_USER_MODEL
 class Tweet(models.Model):
     # id = models.AutoField(PRIMARY_KEY=True) Automaticaly added by django
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
     content = models.TextField(blank=True, null=True) # null for DB, blank for django
     image = models.FileField(upload_to='images/', blank=True, null=True)
     class Meta:
