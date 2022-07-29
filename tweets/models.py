354364f5e8be63d9d5965cc4a7614a8c91fc3cfa
unknown <hussamalkhoury501@gmail.com>
2022-07-29 10:11:37 -0700

    xoneect like t otweet using TweetLike model

--- tweets/models.py
+++ tweets/models.py
@@ -1,13 +1,23 @@
 import random
 from django.conf import settings
+
 from django.db import models
 
 User = settings.AUTH_USER_MODEL
+
+class TweetLike(models.Model):
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    tweet = models.ForeignKey("Tweet", on_delete= models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
 class Tweet(models.Model):
     # id = models.AutoField(PRIMARY_KEY=True) Automaticaly added by django
     user = models.ForeignKey(User, on_delete= models.CASCADE)
     content = models.TextField(blank=True, null=True) # null for DB, blank for django
+    likes = models.ManyToManyField(User, related_name='tweet_user', blank=True, through=TweetLike)
     image = models.FileField(upload_to='images/', blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
     class Meta:
         ordering = ['-id']
         
